1e0a00b427cc91f443bcdc5f0cbf9b5a38bbdb5d
SapnaDilupshi <136107624+SapnaDilupshi@users.noreply.github.com>
2023-07-14 18:02:03 +0530

    Add files via upload
    Lab 10
    WEB Tutorial 
    (2023-07-14)

---
+++ Lab 10-WEB Tutorial (2023-07-14)/Q1.php
@@ -0,0 +1,16 @@
+<?php
+
+$x = 6;
+$y = 4;
+
+$sum = $x + $y."<br>";
+$difference = $x - $y."<br>";
+$product = $x * $y."<br>";
+$division = $x / $y."<br>";
+
+echo "Sum : ".$sum;
+echo "Difference : ".$difference;
+echo "Product : ".$product;
+echo "Division : ".$division;
+
+?>
